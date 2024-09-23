7f2892472ed633261578ed73780688d5194d01cd
Shamanth Ganiger <shamanthganiger@gmail.com>
2024-09-23 12:28:09 +0530

    some padding adjustments for responsiveness

--- frontend/src/Components/AuthorProfile.jsx
+++ frontend/src/Components/AuthorProfile.jsx
@@ -41,12 +41,12 @@ function AuthorProfile() {
   return (
     <div>
       {!loading ? (
-        <div className="px-5 sm:px-16 flex flex-col md:flex-row justify-between border-b-[1px]">
+        <div className="flex flex-col md:flex-row justify-between border-b-[1px]">
           {/* left */}
           <div className="md:hidden block">
-            <img src={"https://res.cloudinary.com/shamanth-ganiger/image/upload/v1727007183/thought-catalog-505eectW54k-unsplash_c8jtsq.jpg"} alt="cover-photo" className="h-[25vh] w-full object-cover" />
+            <img src={"https://res.cloudinary.com/shamanth-ganiger/image/upload/v1727007183/thought-catalog-505eectW54k-unsplash_c8jtsq.jpg"} alt="cover-photo" className="h-[20vh] w-full object-cover" />
           </div>
-          <div className="flex justify-between md:justify-start md:flex-col gap-6 items-start sm:basis-1/4 md:sticky top-20 mt-10 md:h-screen">
+          <div className="px-5 md:pl-16 flex justify-between md:justify-start md:flex-col gap-6 items-start sm:basis-1/4 md:sticky top-20 mt-10 md:h-screen">
             <div className="flex items-center md:flex-col gap-5">
               <img
                 src={author.image}
@@ -80,12 +80,12 @@ function AuthorProfile() {
 
       <div className="w-[1px] bg-gray-300 hidden sm:block"></div>
           {/* right */}
-          <div className="basis-4/6 flex flex-col gap-10 md:pb-10">
+          <div className="basis-4/6 flex flex-col gap-10 px-5 pb-10 md:pl-0 md:pr-16">
           <div className="hidden md:block">
-            <img src={"https://res.cloudinary.com/shamanth-ganiger/image/upload/v1727007183/thought-catalog-505eectW54k-unsplash_c8jtsq.jpg"} alt="cover-photo" className="h-[25vh] w-full object-cover" />
+            <img src={"https://res.cloudinary.com/shamanth-ganiger/image/upload/v1727007183/thought-catalog-505eectW54k-unsplash_c8jtsq.jpg"} alt="cover-photo" className="h-[20vh] w-full object-cover" />
           </div>
             <div className="">
-              <h1 className="text-4xl text-gray-800 font-semibold hidden md:block">
+              <h1 className="text-4xl mt-10 text-gray-800 font-semibold hidden md:block">
                 {author.name}
               </h1>
             </div>
@@ -112,7 +112,7 @@ function AuthorProfile() {
               </button>
             </div>
 
-            <div className='grid pb-10 grid-cols-[repeat(auto-fit,_minmax(250px,_310px))] gap-10'>
+            <div className='grid grid-cols-[repeat(auto-fit,_minmax(250px,_310px))] gap-10'>
               {detail === "home" && author.blogs.length !== 0
                 ? author.blogs.map((blog, index) => {
                     return <Blogs key={index} blog={blog} />
