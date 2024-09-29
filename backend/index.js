c41dbc56c89bfda4a8a71a9bb0b27335df00578f
Shamanth Ganiger <shamanthganiger@gmail.com>
2024-09-29 14:58:11 +0530

    origin cahnge

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,8 @@ connectDB();
 // }))
 
 app.use(cors({
-    origin: 'http://localhost:5173', // Allow requests from this origin
+    // origin: 'http://localhost:5173', // Allow requests from this origin
+    origin: 'http://192.168.1.3:5173', // Allow requests from this origin
     credentials: true // Allow credentials (cookies, authorization headers)
 }));
 
