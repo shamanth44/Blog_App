107587208deb9433cc107dcf237cc3c1d936d42d
Shamanth Ganiger <shamanthganiger@gmail.com>
2024-09-29 17:59:29 +0530

    origin test

--- backend/index.js
+++ backend/index.js
@@ -19,9 +19,9 @@ connectDB();
 // }))
 
 app.use(cors({
-    // origin: 'http://localhost:5173', // Allow requests from this origin
-    origin: 'http://192.168.1.4:5173', // Allow requests from this origin
-    credentials: true // Allow credentials (cookies, authorization headers)
+    // origin: 'http://localhost:5173',
+    origin: 'http://192.168.1.4:5173', 
+    credentials: true
 }));
 
 app.use(bodyParser.json())
