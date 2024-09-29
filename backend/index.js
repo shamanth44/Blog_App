4f3083bce2c01e92e77672999280d8e34690d3e2
Shamanth Ganiger <shamanthganiger@gmail.com>
2024-09-29 14:47:26 +0530

    again cors

--- backend/index.js
+++ backend/index.js
@@ -18,12 +18,10 @@ connectDB();
 //    credentials: true
 // }))
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173'); // Allow requests from your frontend
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Allow specific HTTP methods
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Allow specific headers
-    next();
-  });
+app.use(cors({
+    origin: 'http://localhost:5173', // Allow requests from this origin
+    credentials: true // Allow credentials (cookies, authorization headers)
+}));
 
 app.use(bodyParser.json())
 app.use(express.json())
